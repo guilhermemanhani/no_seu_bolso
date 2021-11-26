a6e1d83cd29c9f75aa34c44d125e610e262dd70d
Guilherme Augusto da Rocha Manhani <gmanhani@sup05.unicred-parana.corp>
2021-11-26 14:08:25 -0300

    tableas com foreign key funcionando

--- lib/app/core/database/migration/migration_v1.dart
+++ lib/app/core/database/migration/migration_v1.dart
@@ -5,27 +5,20 @@ class MigrationV1 implements Migration {
   @override
   void create(Batch batch) {
     batch.execute('''
-
-  CREATE TABLE banco (
-    idbanco INT NOT NULL,
-    bancocol VARCHAR(45) NULL,
-    PRIMARY KEY (idbanco)
-    )
-
-  CREATE TABLE lancamento (
-    idlancamento INT NOT NULL AUTO_INCREMENT,
-    valor DECIMAL(100) NULL,
-    lancamentocol VARCHAR(45) NULL,
-    banco_idbanco INT NOT NULL,
-    PRIMARY KEY (idlancamento),
-    INDEX fk_lancamento_banco_idx (banco_idbanco ASC) VISIBLE,
-    CONSTRAINT fk_lancamento_banco
-      FOREIGN KEY (banco_idbanco)
-      REFERENCES banco (idbanco)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-    )
-    ''');
+          CREATE TABLE banco (
+            id Integer primary key autoincrement,
+            instituicao VARCHAR(45) not null    
+          )
+        ''');
+    batch.execute('''
+          CREATE TABLE lancamento (
+            idlancamento Integer primary key autoincrement,
+            valor REAL NOT NULL,
+            descricao VARCHAR(45) NULL,
+            idbanco Interger NOT NULL,
+            FOREIGN KEY (idbanco) REFERENCES banco (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+          )
+        ''');
   }
 
   @override
@@ -39,9 +32,12 @@ class MigrationV1 implements Migration {
 //     )
 
 // CREATE TABLE MarketInfoes ("
-//           "id integer primary key, userId integer, typeId integer,"
-//           "gradeId integer, price DOUBLE" 
-//           "FOREIGN KEY (typeId) REFERENCES Type (id) ON DELETE NO ACTION ON UPDATE NO ACTION," 
-//           "FOREIGN KEY (userId) REFERENCES User (id) ON DELETE NO ACTION ON UPDATE NO ACTION," 
-//           "FOREIGN KEY (gradeId) REFERENCES Grade (id) ON DELETE NO ACTION ON UPDATE NO ACTION"
-//           ")
+//      "id integer primary key, 
+//      userId integer, 
+//      typeId integer,"
+//      "gradeId integer,
+//       price DOUBLE" 
+//      "FOREIGN KEY (typeId) REFERENCES Type (id) ON DELETE NO ACTION ON UPDATE NO ACTION," 
+//      "FOREIGN KEY (userId) REFERENCES User (id) ON DELETE NO ACTION ON UPDATE NO ACTION," 
+//      "FOREIGN KEY (gradeId) REFERENCES Grade (id) ON DELETE NO ACTION ON UPDATE NO ACTION"
+//    ")
